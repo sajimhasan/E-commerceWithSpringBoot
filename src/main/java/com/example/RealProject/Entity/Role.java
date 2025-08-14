e169423f16e0a87a914647624d7f127c4e44cdcc
sajimhasan <sim25642@gmail.com>
2025-08-14 11:35:56 +0600

    After the running point

--- src/main/java/com/example/RealProject/Entity/Role.java
+++ src/main/java/com/example/RealProject/Entity/Role.java
@@ -1,7 +1,16 @@
 package com.example.RealProject.Entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 public enum Role {
     ADMIN,
     USER,
     SELLER;
+
+    @JsonCreator
+    public static Role fromString(String value) {
+        return Role.valueOf(value.toUpperCase());
+    }
 }
